beac43a761f462039ba87fdee102fe2f26269f68
Ankhbayar <ankhbayar.sukhee@gmail.com>
2023-07-28 15:14:41 +0800

    AshidDental program update hiih uildeltei holbootoi hugjuulelt hiiv.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
 Route::get('/', function () {
     return view('home');
 });
@@ -20,6 +23,42 @@ Route::get('#contact', function () {
 
 Route::get('/test', function () {
     return view('welcome');
+});
+
+Route::get('/checkforupdates/ashiddental', function (Request $request) {
+    // Check if the request contains the custom header or parameter required for authentication
+    // Replace 'YOUR_CUSTOM_HEADER' with the actual header name or 'YOUR_PARAM_NAME' with the actual parameter name used by your Windows application.
+    if ($request->header('User-Agent') === 'AutoUpdaterDotNet' || $request->query('ashiddental') === 'AshidSec1-9*') {
+        // The secret key matches, so return the XML file as the response
+        return Response::file(storage_path('xml/ashiddental.xml'));
+    } else {
+        // Unauthorized access, return a 404 response or any other response you prefer
+        abort(404);
+    }
+});
+
+Route::get('/products/ashiddental/setup', function (Request $request) {
+    $filePath = storage_path('/products/ashiddental/SetupProject.msi');
+
+    if ($request->header('User-Agent') === 'AutoUpdaterDotNet' && file_exists($filePath) && is_file($filePath)) {
+        // The secret key matches, so return the XML file as the response
+        return response()->download($filePath);
+    } else {
+        // If the file does not exist, return a 404 response or any other response you prefer
+        abort(404);
+    }
+});
+
+Route::get('/products/ashiddental/SetupProject.msi', function (Request $request) {
+    $filePath = public_path('/products/ashiddental/SetupProject.msi');
+        
+    if (file_exists($filePath) && is_file($filePath)) {
+        // Return the file as a download response
+        return response()->download($filePath);
+    } else {
+        // If the file does not exist, return a 404 response or any other response you prefer
+        abort(404);
+    }
 });
 
 Route::get('/contact', 'SendEmailController@index');
